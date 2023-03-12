4b56cccebf8c44ce706e48eaafabde30211370da
HARAJIT DAS <110291590+HARAJIT05@users.noreply.github.com>
2023-03-12 20:43:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
 #python code for sending WhatsApp message to a specific number in a specific time
 import pywhatkit
-pywhatkit.sendwhatmsg("+1234567890", "replace this with your message", 12, 48)
-    # replace the phone number with the  phone number of the recipient
+number = input("Enter the phone number with country code: ")
+message = input("Enter the message: ")
+hour = int(input("Enter the Hour in 24 hour format: "))
+minute = int(input("Enter the Minute: "))
+pywhatkit.sendwhatmsg(number, message, hour, minute)
+#owner:@Harajit
